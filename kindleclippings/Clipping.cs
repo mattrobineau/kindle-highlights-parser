bc7dab115454fa016a5e9a24eae9e416f6d4fc96
Matt Robineau <matthieu.robineau@gmail.com>
2019-10-15 21:37:40 -0400

    Init Commit - Read test.txt (My Clippings.txt) - Parse lines into Clippings class - Convert to json - Write to file

---
+++ kindleclippings/Clipping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kindleclippings
+{
+    public class Clipping
+    {
+        public string Author { get; set; }
+        public DateTime HighlightDate { get; set; }
+        public string Quote { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ kindleclippings/Program.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace kindleclippings
+{
+    internal static class Program
+    {
+#pragma warning disable RCS1163 // Unused parameter.
+        static void Main(string[] args)
+#pragma warning restore RCS1163 // Unused parameter.
+        {
+            (string title, string author) ParseTitleLine(string line)
+            {
+                return (title: line.Substring(0, line.LastIndexOf("(")), line.Substring(line.LastIndexOf("(")));
+            }
+
+            using (var file = new System.IO.StreamReader(@"test.txt"))
+            {
+                List<Clipping> clippings = new List<Clipping>();
+
+                string line;
+                int lineCount = 0;
+
+                var clipping = new Clipping();
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    lineCount++;
+
+                    if (lineCount % 5 == 0)
+                    {
+                        clippings.Add(clipping);
+                        clipping = new Clipping();
+                    }
+                    try
+                    {
+                        switch (lineCount % 5)
+                        {
+                            case 1:
+                                (clipping.Title, clipping.Author) = ParseTitleLine(line);
+                                break;
+                            case 2:
+                                var val = line.Split('|').Last();
+                                clipping.HighlightDate = DateTime.Parse(val.Replace("Added on", "").Trim());
+                                break;
+                            case 3:
+                                // Ln 3 is newline
+                                continue;
+                            case 4:
+                                clipping.Quote = line.Trim();
+                                break;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($"Failed to parse line {lineCount}");
+                    }
+                }
+
+                using (StreamWriter outputFile = new StreamWriter("quotes.json"))
+                {
+                    outputFile.WriteLine(JsonConvert.SerializeObject(clippings));
+                }
+            }
+        }
+    }
+}
